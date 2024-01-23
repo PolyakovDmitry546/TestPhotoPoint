9f9595620127d617ab1ca300103027022514dd55
PolyakovDmitry546 <65275577+PolyakovDmitry546@users.noreply.github.com>
2024-01-23 18:26:49 +0500

    Add interval for request exchange rate

--- src/exchange_rates/services.py
+++ src/exchange_rates/services.py
@@ -1,15 +1,40 @@
+from datetime import timedelta
+
+from django.conf import settings
+from django.utils import timezone
+
 from exchange_rates.models import ExchangeRate
 from exchange_rates_providers.openexchangerates.provider import OpenExchangeRatesProvider
 
 
+EXCHANGE_RATE_REQUEST_INTERVAL = settings.EXCHANGE_RATE_REQUEST_INTERVAL
+
+
 class ExchangeRatesService:
+    def _rate_request_interval_has_expired(
+            self, last_exchange_rate: ExchangeRate) -> bool:
+        """Возвращает True, если истек интервал
+          для запроса нового курса валют (EXCHANGE_RATE_REQUEST_INTERVAL)"""
+        interval = timedelta(seconds=EXCHANGE_RATE_REQUEST_INTERVAL)
+        return last_exchange_rate.create_at + interval < timezone.now()
+
+    def _get_rate_from_provider_and_save_to_db(self):
+        rate = OpenExchangeRatesProvider.get_usd_rub_rate()
+        ExchangeRate.objects.create(currency_pair='USDRUB',
+                                    rate=rate)
+        return rate
+
     def get_last_ten_usd_rub_rates(self) -> list[float]:
         """Возвращает последние десять курсов валют USDRUB"""
-        rates = ExchangeRate.objects.all().order_by('-id')[:10]
-        rates = [rate.rate for rate in rates]
+        rates_query = ExchangeRate.objects.all().order_by('-id')[:10]
+        rates = [rate.rate for rate in rates_query]
         if len(rates) < 1:
-            rate = OpenExchangeRatesProvider.get_usd_rub_rate()
-            ExchangeRate.objects.create(currency_pair='USDRUB',
-                                        rate=rate)
+            rate = self._get_rate_from_provider_and_save_to_db()
             rates.append(rate)
+            return rates
+
+        if self._rate_request_interval_has_expired(rates_query[0]):
+            rate = self._get_rate_from_provider_and_save_to_db()
+            rates = [rate] + rates[:9]
+
         return rates
--- src/tests/test_exchange_rates/test_services.py
+++ src/tests/test_exchange_rates/test_services.py
@@ -1,7 +1,13 @@
+from datetime import timedelta
+
+from django.conf import settings
 from django.test import TestCase
 
 from exchange_rates.models import ExchangeRate
 from exchange_rates.services import ExchangeRatesService
+
+
+EXCHANGE_RATE_REQUEST_INTERVAL = settings.EXCHANGE_RATE_REQUEST_INTERVAL
 
 
 class TestExchangeRatesService(TestCase):
@@ -20,7 +26,29 @@ class TestExchangeRatesService(TestCase):
         expected_rates = self.last_ten_usd_rub_rates
         service = ExchangeRatesService()
         rates = service.get_last_ten_usd_rub_rates()
+        self.assertTrue(len(rates) <= 10)
         self.assertListEqual(expected_rates, rates)
+
+    def test_rate_request_interval_has_expired(self):
+        last_rate = ExchangeRate.objects.all().order_by('-id').first()
+        interval = timedelta(seconds=EXCHANGE_RATE_REQUEST_INTERVAL + 1000)
+        last_rate.create_at = last_rate.create_at - interval
+        last_rate.save()
+
+        service = ExchangeRatesService()
+        self.assertTrue(service._rate_request_interval_has_expired(last_rate))
+
+    def test_get_last_ten_usd_rub_rates_with_expired_request_interval(self):
+        last_rate = ExchangeRate.objects.all().order_by('-id').first()
+        interval = timedelta(seconds=EXCHANGE_RATE_REQUEST_INTERVAL + 1000)
+        last_rate.create_at = last_rate.create_at - interval
+        last_rate.save()
+
+        not_expected_rates = self.last_ten_usd_rub_rates
+        service = ExchangeRatesService()
+        rates = service.get_last_ten_usd_rub_rates()
+        self.assertTrue(len(rates) <= 10)
+        self.assertNotEqual(rates[0], not_expected_rates[0])
 
 
 class TestExchangeRatesServiceWithEmptyDB(TestCase):
