6e92f992ceeff45b75288d330301eb940b5e9f85
PolyakovDmitry546 <65275577+PolyakovDmitry546@users.noreply.github.com>
2024-01-23 18:28:28 +0500

    Add handling exceptions to view

--- src/exchange_rates/views.py
+++ src/exchange_rates/views.py
@@ -7,6 +7,11 @@ from exchange_rates.services import ExchangeRatesService
 class CurrentUSDRUBRateView(View):
     def get(self, request, *args, **kwargs):
         service = ExchangeRatesService()
-        rates = service.get_last_ten_usd_rub_rates()
+        try:
+            rates = service.get_last_ten_usd_rub_rates()
+        except Exception:
+            return JsonResponse({'message': 'Internal server error'},
+                                status=500)
+
         return JsonResponse({'current_USD_RUB_rate': rates[0],
                             'last_ten_USD_RUB_rates': rates})
